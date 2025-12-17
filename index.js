c4b0fe5c6ea7afb16a85f03dc0557237ff8b9828
Sakif Ahmed <ahmed.sakif225@gmail.com>
2025-12-17 15:21:31 +0600

    Added user endpoint based on role

--- index.js
+++ index.js
@@ -22,19 +22,71 @@ const client = new MongoClient(uri, {
 
 
 app.get('/', (req, res) => {
-    res.send("AssetVerse server is running")
+  res.send("AssetVerse server is running")
 })
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db("AssetVerse_DB")
+    const usersCollection = db.collection('users')
 
-    app.get('/assets', (req,res)=>{
-        res.send("Inside Mongo DB")
+
+    app.get('/assets', (req, res) => {
+      res.send("Inside Mongo DB")
     })
 
+    app.post('/users', async (req, res) => {
+      const userData = req.body
+
+      // Social Login Protection
+      const query = { email: userData.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'User already exists', insertedId: null })
+      }
+
+      // Based on Role
+
+      let newUser = {}
+
+      if (userData.role === 'hr') {
+        // HR Registration
+        newUser = {
+          name: userData.name,
+          email: userData.email,
+          companyName: userData.companyName,
+          companyLogo: userData.companyLogo,
+          dateOfBirth: userData.dateOfBirth,
+          userPhoto: userData.userPhoto,
+          // Auto-assigned Fields
+          role: 'hr',
+          packageLimit: 5,
+          currentEmployees: 0,
+          subscription: 'basic'
+        }
+      } 
+      if (userData.role === 'employee') {
+        //Employee Registration   
+        newUser = {
+          name: userData.name,
+          email: userData.email,
+          dateOfBirth: userData.dateOfBirth,
+          userPhoto: userData.userPhoto,
+          // Auto-assigned
+          role: 'employee'
+        }
+      }
+
+      // Save to MongoDB
+      const result = await usersCollection.insertOne(newUser)
+      res.send(result)
+      console.log(userData);
+
+    })
+
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -46,5 +98,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`AssetVerse server is runner on port: ${port}`);
+  console.log(`AssetVerse server is runner on port: ${port}`);
 })
